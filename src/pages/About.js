cdc8a343bc041fd26b2eb0d1762e9959e7c1a9ec
JulienForesier <forestierjulien3@gmail.com>
2023-04-06 16:10:49 +0200

    Création du p7

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <Navigation />
+    </div>
+  );
+};
+
+export default About;
