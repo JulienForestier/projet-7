04f921fbd99324c55ebc6a9328e5441307641bca
JulienForesier <forestierjulien3@gmail.com>
2023-04-17 21:17:05 +0200

    desktop ready

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,43 +5,28 @@ import Rate from "./Rate";
 const Description = ({ logement }) => {
   return (
     <section className="description">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "100%",
-        }}
-      >
-        <div className="description-header">
-          <h2>{logement.title}</h2>
-          <p>{logement.location}</p>
-        </div>
-        <div className="host">
-          <div>
-            <p>{logement.host.name}</p>
-            <img
-              src={logement.host.picture}
-              alt="photographie du propriétaire"
-              style={{ height: "80px" }}
-            />
-          </div>
-        </div>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "100%",
-          height: "30px",
-          marginTop: "10px",
-        }}
-      >
+      <div className="description-header">
+        <h2>{logement.title}</h2>
+        <p>{logement.location}</p>
         <div className="tags">
           {logement.tags.map((tag) => {
             return <Tag content={tag} key={[tag]} />;
           })}
         </div>
-        <Rate rate={logement.rating} />
+      </div>
+
+      <div className="host">
+        <div>
+          <p>{logement.host.name}</p>
+          <img
+            src={logement.host.picture}
+            alt="photographie du propriétaire"
+            style={{ height: "80px" }}
+          />
+        </div>
+        <div className="rate">
+          <Rate rate={logement.rating} />
+        </div>
       </div>
     </section>
   );
